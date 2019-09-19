0ddd5369825405f4f8c86283fd48be92d8bbeedb
yuta-ksksr <yuta.tkhs.h2o@gmail.com>
2019-09-20 01:24:59 +0900

    S3 setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -98,10 +98,9 @@ namespace :deploy do
 end
 
 
-
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AKIAXWNVGF2H7IHZAMKG"],
-  AWS_SECRET_ACCESS_KEY: ENV["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"]
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
