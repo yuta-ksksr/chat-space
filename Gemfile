2bfca3d41bffbd2e3c35d6b896ca65dd2aa2385c
yuta-ksksr <yuta.tkhs.h2o@gmail.com>
2019-09-20 00:28:15 +0900

    setting S3

--- Gemfile
+++ Gemfile
@@ -71,3 +71,4 @@ gem 'pry-rails'
 group :production do
   gem 'unicorn', '5.4.1'
 end
+gem 'fog-aws'
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,11 +1,11 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class Uploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -18,7 +18,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  #   "/s/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
   # Process files as they are uploaded:
@@ -34,7 +34,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
+  # For s you might use something like this:
   # def extension_whitelist
   #   %w(jpg jpeg gif png)
   # end
