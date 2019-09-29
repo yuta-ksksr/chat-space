f08257601077bdf7d122b22066a24b4c4b0fedb6
yuta-ksksr <yuta.tkhs.h2o@gmail.com>
2019-09-29 16:51:40 +0900

    時間表示

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,7 @@ module ChatSpace
       g.helper false
       g.test_framework false
       # config.i18n.default_locale = :ja
-      config.time_zone = 'Tokyo'
-      config.active_record.default_timezone = :local
+      config.time_zone = 'Asia/Tokyo'
     end
   end
 end
